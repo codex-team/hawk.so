129bf9a20d2f14b97c75b84e1ffcc0e9fa508fa4
Nikita Melnikov <nikmel2803@gmail.com>
2019-03-27 09:43:55 +0300

    remove entrypoint

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js'
-    },
     module: {
         rules: [
             {test: /\.twig$/, loader: "twig-loader"}
