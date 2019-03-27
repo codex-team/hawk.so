43f45967fabb40930f0f64be0ccf20c3fde1f0ad
Nikita Melnikov <nikmel2803@gmail.com>
2019-03-27 10:07:40 +0300

    add css building

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,31 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    entry: './src/index.js',
     module: {
         rules: [
-            {test: /\.twig$/, loader: "twig-loader"}
+            {
+                test: /\.twig$/,
+                loader: "twig-loader"
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader'
+                ]
+            }
         ]
     },
 
     plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'index.css'
+        }),
         new HtmlWebpackPlugin({
             template: path.resolve('./src/views/index.twig'),
             filename: path.resolve('./dist/index.html'),
