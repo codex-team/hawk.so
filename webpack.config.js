a07803ab140635f818f714e83f6f7103469cd073
Nikita Melnikov <nikmel2803@gmail.com>
2019-03-27 10:14:18 +0300

    add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -30,7 +31,14 @@ module.exports = {
             template: path.resolve('./src/views/index.twig'),
             filename: path.resolve('./dist/index.html'),
             inject: true
-        })
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: './src/assets/**/*',
+                to: './assets/[name].[ext]',
+                test: /\.(png|jpg|gif|svg|woff|woff2|ttf|eot)$/
+            }
+        ])
     ]
 }
 ;
