d1fec7920b94cba243887e6656b652455e5ca3b4
Peter Savchenko <specc.dev@gmail.com>
2020-12-19 17:55:54 +0300

    up title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'hawk-yard',
+    title: 'Hawk. Errors catcher. Reimagined.',
     meta: [
       { charset: 'utf-8' },
       {
